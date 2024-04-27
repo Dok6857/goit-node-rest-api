970db0a824a6ab2a26d9c584ebbd01c23fb0b196
Dok6857 <Dok6857@gmail.com>
2024-04-27 23:32:05 +0300

    make createContact func

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,6 +5,10 @@ import {
   addContact,
 } from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res, next) => {
   const contacts = await listContacts();
@@ -30,15 +34,33 @@ export const deleteContact = async (req, res, next) => {
     const { id } = req.params;
     const removedContact = await removeContact(id);
     if (removedContact) {
-      res.status(200).json(removedContact)
+      res.status(200).json(removedContact);
     } else {
-      throw HttpError(404)
+      throw HttpError(404);
     }
   } catch (error) {
     next(error);
   }
 };
 
-export const createContact = async (req, res, next) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const schema = createContactSchema.validate(req.body)
+    const { name, email, phone } = req.body;
+    if (schema.error) {
+      throw HttpError(400, schema.error.message)
+    }
+    const newContact = await addContact(name, email, phone)
+    res.status(201).json(newContact)
+  } catch (error) {
+    next(error)
+  }
+};
 
-export const updateContact = async (req, res, next) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
+};
