894e0be71060a78c1f79ea7c24cabbe757ee59f0
Dok6857 <Dok6857@gmail.com>
2024-05-17 17:41:41 +0300

    move dotenv to package.json

--- configs/serverConfig.js
+++ configs/serverConfig.js
@@ -1,9 +1,5 @@
 // serverConfig
 
-import dotenv from "dotenv";
-
-dotenv.config();
-
 export const {
   PORT = 3000,
   DB_URI = `mongodb+srv://dok6857:rpCgMcjc9zjN0sAp@cluster0.okqbwbp.mongodb.net/db-contacts
