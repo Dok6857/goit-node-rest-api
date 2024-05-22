1b8cc5c5e97ef6860adb3bd0ac402509d14f76f3
Dok6857 <Dok6857@gmail.com>
2024-05-22 16:08:43 +0300

    add avatarURL

--- models/user.js
+++ models/user.js
@@ -2,28 +2,35 @@
 
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  password: {
-    type: String,
-    required: [true, "Password is required"],
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    avatarURL: {
+      type: String,
+      default: null,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: {
-    type: String,
-    default: null,
-  },
-}, {
-  versionKey: false,
-});
+  {
+    versionKey: false,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
