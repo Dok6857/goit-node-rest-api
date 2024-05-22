5b8331d4016c25574eca5e05cbf8c311b1a5bd96
Dok6857 <Dok6857@gmail.com>
2024-05-22 16:09:35 +0300

    add route

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.Promise = global.Promise;
 
 import contactsRouter from "./routes/contactsRouter.js";
 import authRouter from "./routes/auth.js";
+import usersRouter from "./routes/users.js";
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
+app.use("/api/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
