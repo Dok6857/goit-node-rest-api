49309273959528c7efd62b7d3655478544f88340
Dok6857 <Dok6857@gmail.com>
2024-05-28 16:30:54 +0300

    add email schema

--- schemas/userValidationSchema.js
+++ schemas/userValidationSchema.js
@@ -5,3 +5,7 @@ export const userRegisterSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
+
+export const emailSchema = Joi.object({
+  email: Joi.string().email().required().lowercase().trim(),
+});
